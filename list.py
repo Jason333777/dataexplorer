8de460dabba6574421eb29ca07a274bb4d8fc78c
Jason333777 <yilan086@gmail.com>
2024-10-07 18:25:27 -0700

    completed excersie 1 of project 2

---
+++ list.py
@@ -0,0 +1,9 @@
+movies = ['Spiderman', 'Iron Man', 'Godzilla x Kong', 'Minions']
+ratings = [7.3, 7.9, 6.1, 6.4]
+genres = ['comedy', 'action', 'action', 'comedy']
+ratings[0]= 7.4
+print(ratings[0])
+movies.append('Lion King')
+ratings.append(8.5)
+genres.append('drama')
+print(movies[4])
