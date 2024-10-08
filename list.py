dd6d8c1036f647049723324487397ba6c56ebe99
Jason333777 <yilan086@gmail.com>
2024-10-07 18:36:39 -0700

    completed exercise 1 in project 2

--- list.py
+++ list.py
@@ -1,8 +1,13 @@
+#making lists of movies and their ratings and genre
 movies = ['Spiderman', 'Iron Man', 'Godzilla x Kong', 'Minions']
 ratings = [7.3, 7.9, 6.1, 6.4]
 genres = ['comedy', 'action', 'action', 'comedy']
+
+#changing the rating of spiderman
 ratings[0]= 7.4
 print(ratings[0])
+
+#adding lion king to the list
 movies.append('Lion King')
 ratings.append(8.5)
 genres.append('drama')
